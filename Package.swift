5704b04e986912772cd086a3b5ba4662edd41fc0
Jonathan Guthrie <jono@perfect.org>
2016-12-30 16:04:57 -0500

    using 2.1

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-12-19 19:52:53 +0000
+// Date: 2016-12-30 20:59:27 +0000
 import PackageDescription
 let package = Package(
     name: "PerfectSessionMySQL",
     targets: [],
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 0, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2, minor: 1),
     ]
 )
