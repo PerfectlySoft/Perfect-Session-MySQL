74f9b65d898f57d6450dfeea9f8e1dc1f5f97c70
Kyle Jessup <kyle.jessup@treefrog.ca>
2019-03-26 13:51:43 -0400

    Updated package file

--- Package.swift
+++ Package.swift
@@ -1,11 +1,17 @@
+// swift-tools-version:4.2
 // Generated automatically by Perfect Assistant Application
 // Date: 2017-10-03 21:14:19 +0000
 import PackageDescription
 let package = Package(
 	name: "PerfectSessionMySQL",
-	targets: [],
+	products: [
+		.library(name: "PerfectSessionMySQL", targets: ["PerfectSessionMySQL"])
+	],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 3),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", from: "3.0.0"),
+	],
+	targets: [
+		.target(name: "PerfectSessionMySQL", dependencies: ["PerfectSession", "PerfectMySQL"])
 	]
 )
